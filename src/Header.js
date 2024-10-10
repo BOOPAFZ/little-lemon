4a1fd06efca8dd505dad5e7f65d90e113d5c3ea5
BOOPAFZ <bakhsoussa@gmail.com>
2024-10-10 18:09:49 -0400

    Refactor App.js with semantic HTML structure, create Header, Nav, Main, Footer components, add meta tags for SEO and Open Graph protocol

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from './assets/logo.png';
+
+
+
+function Header() {
+    return <header>
+        <img src={logo} alt="Little Lemon Logo" />
+    </header>;
+}
+
+
+export default Header;

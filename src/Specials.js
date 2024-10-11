996da3083736b4c0b23ee3044ec02ddac7d86a81
BOOPAFZ <bakhsoussa@gmail.com>
2024-10-11 01:58:42 -0400

    imgs fixed

--- src/Specials.js
+++ src/Specials.js
@@ -13,37 +13,36 @@ function Specials() {
         <div className="card">
           <img className="card-img-top" src="/assets/tajine.jpg" alt="Dish Image" />
           <div className="card-body">
-            <h5 className="card-title">Dish Title</h5>
+            <h5 className="card-title">Tagine</h5>
             <p className="card-text">
-              A delicious description of the dish. It can be savory, sweet, or spicy depending on your preference.
+            is a traditional North African dish 
             </p>
             <button className="btn btn-primary">Order Now</button>
           </div>
         </div>
 
         <div className="card">
-          <img className="card-img-top" src="/assets/tajine.jpg" alt="Dish Image" />
+          <img className="card-img-top" src="/assets/bastila.jpg" alt="Dish Image" />
           <div className="card-body">
-            <h5 className="card-title">Dish Title</h5>
+            <h5 className="card-title">Moroccan Chicken Bastila</h5>
             <p className="card-text">
-              Another tempting dish with a unique blend of flavors to satisfy your taste buds.
+            a traditional Moroccan pie that beautifully combines sweet and savory flavors. 
             </p>
             <button className="btn btn-primary">Order Now</button>
           </div>
         </div>
 
         <div className="card">
-          <img className="card-img-top" src="/assets/tajine.jpg" alt="Dish Image" />
+          <img className="card-img-top" src="/assets/omlette.jpg" alt="Dish Image" />
           <div className="card-body">
-            <h5 className="card-title">Dish Title</h5>
+            <h5 className="card-title">Persian Omelette</h5>
             <p className="card-text">
-              Another tempting dish with a unique blend of flavors to satisfy your taste buds.
+            Omelette Irani, is a simple yet flavorful dish that is perfect for any meal of the day.
             </p>
             <button className="btn btn-primary">Order Now</button>
           </div>
         </div>
 
-        
 
 
       </div>
